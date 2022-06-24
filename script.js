c9937931382027a406bac492f6f52c688a16aa66
Ronak Jain <ronak29jain@gmail.com>
2022-06-24 09:52:43 +0530

    First stage complete

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 var firstcolor = document.getElementById('firstcolor');
 var secondcolor = document.getElementById('secondcolor');
+var h3 = document.querySelector('h3');
+h3.innerHTML = "Linear Gradient of color " + firstcolor.value + ' and ' + secondcolor.value;
 
-var body = document.getElementsByTagName('body')[0];
+var body = document.querySelector('body');
 
-function print(){
-    console.log(firstcolor.value, secondcolor.value)
-    body.style.background = linear-gradient(to left, firstcolor.value, secondcolor.value); 
+function linear_gradient(){
+    // console.log(firstcolor.value, secondcolor.value);
+    body.style.background = "linear-gradient(to right, " + firstcolor.value + "," + secondcolor.value + ")"; 
+    h3.innerHTML = "Linear Gradient of color " + firstcolor.value + ' and ' + secondcolor.value;
 }
 
-print();
+firstcolor.addEventListener('input', linear_gradient);
+secondcolor.addEventListener('input', linear_gradient);
